5d0121e56aecb3140807ea2abbfa0a1eda33b1db
piees <yngveranestad@gmail.com>
2016-01-08 01:30:27 +0100

    format fixes

--- soup.py
+++ soup.py
@@ -4,7 +4,8 @@ import re
 
 
 
-URL = "http://timeplan.uia.no/swsuiav/XMLEngine/default.aspx?ModuleByWeek&p1=;IS-213-1;&p2=0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23"
+#URL = "http://timeplan.uia.no/swsuiav/XMLEngine/default.aspx?ModuleByWeek&p1=;IS-213-1;&p2=0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23"
+URL = "http://timeplan.uia.no/swsuiav/XMLEngine/default.aspx?ModuleByWeek&p1=;IS-211-1;&p2=0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23"
 
 response = requests.get(URL)
 
@@ -12,32 +13,11 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 tr = soup.find_all('tr')
 
-def testSplitTr():
-	print " "
-	print "splitTr 0"
-	for x in splitTr[0]:
-		print x
-
-	print " "
-	print "splitTr 1"
-	for x in splitTr[1]:
-		print x
-
-	print " "
-	print "splitTr 2"
-	for x in splitTr[2]:
-		print x
-
-	print " "
-	print "splitTr 3"
-	for x in splitTr[3]:
-		print x
 
 def textDateToInt(txtDate):
 	for index, item in enumerate(calendar.month_name):
 		if item == txtDate:
 			return index
-
 
 splitTr = []
 processedTr = []
@@ -46,7 +26,6 @@ finalTr = []
 for x in tr:
 	s = str(x).split("<td")
 	splitTr.append(s)
-		#splitTr.append(x.split("<td>"))
 
 
 # remove non-class tr
@@ -66,6 +45,13 @@ for x in splitTr:
 		temp.append(y.replace("</td>", ""))
 	processedTr.append(temp)
 
+#for x in processedTr:
+#	print len(x)
+#for x in processedTr[0]:
+#	print "n"
+#	print x
+
+
 for x in processedTr:
 	try:
 		dHold = {}
@@ -76,14 +62,18 @@ for x in processedTr:
 		# append time
 		dHold["time"] = x[3][15:]
 		# append course name
-		dHold["course"] = x[4][3:]
+		dHold["course"] = x[4][2:]
+		# append room
+		dHold["room"] = x[5][1:]
 		# append teacher name
-		dHold["tName"] = x[5][1:]
+		dHold["tName"] = x[6][1:]
 		finalTr.append(dHold)
 	except:
 		pass
 
 #testSplitTr()
+
+#print finalTr[0]
 
 for x in finalTr:
 	print x
