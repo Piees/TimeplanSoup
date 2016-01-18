7d79687098a75a23db1495f105be3cf957ec96a9
piees <yngveranestad@gmail.com>
2016-01-18 12:16:29 +0100

    scrape coursecodes into dict

---
+++ seleniumtest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+browser.get('http://timeplan.uia.no/swsuiav/public/no/default.aspx')
+browser.execute_script('__doPostBack(\'LinkBtn_modules\',\'\')')
+elements = browser.find_elements_by_xpath("//select[@class='DepartmentFilter']/option")
+strElements = {}
+for x in elements:
+    temp = x.get_attribute('innerHTML').split(' ', 1)
+    if len(temp[0]) > 5:
+        if temp[0][1].isupper() and temp[0][len(temp[0])-3].isdigit():
+            strElements[temp[0]] = temp[1]
+for x in strElements:
+    print x
+browser.close()
