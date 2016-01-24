1dc60825e2fb1a199c5b74c0260892428e39121b
piees <yngveranestad@gmail.com>
2016-01-24 01:22:40 +0100

    deprecate

--- seleniumtest.py
+++ seleniumtest.py
@@ -6,6 +6,11 @@ browser = webdriver.Firefox()
 browser.get('http://timeplan.uia.no/swsuiav/public/no/default.aspx')
 browser.execute_script('__doPostBack(\'LinkBtn_modules\',\'\')')
 elements = browser.find_elements_by_xpath("//select[@class='DepartmentFilter']/option")
+#for x in elements:
+#    print ''
+#    print x.get_attribute('innerHTML')
+#    f = x.get_attribute('innerHTML').split(' ', 1)[0]
+#    print f
 strElements = {}
 for x in elements:
     temp = x.get_attribute('innerHTML').split(' ', 1)
@@ -13,3 +18,10 @@ for x in elements:
         if temp[0][1].isupper() and temp[0][len(temp[0])-3].isdigit():
             strElements[temp[0].lower()] = temp[1]
 browser.close()
+
+t = open('txtsplit.txt', 'w')
+t.truncate()
+t.write(str(strElements))
+
+for x in strElements:
+    print x
