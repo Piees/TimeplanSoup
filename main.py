6471caae3a2b62797ddc4031ad805d6c22acc5a8
piees <yngveranestad@gmail.com>
2016-01-25 01:05:43 +0100

    add functionality to choose course from form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, make_response
 # string to dict module
 import ast
 import datetime
@@ -11,23 +11,28 @@ with open('textsoup.txt', 'r') as courses:
 
 courses = ast.literal_eval(courses)
 
-selected = ['is-213', 'is-211', 'is-110', 'is-202']
+#selected = ['is-213', 'is-211', 'is-110', 'is-202']
+selected = []
 
 selCourses = []
 
-for x in courses:
-    for y in selected:
-        if x['course'].lower() == y:
-            selCourses.append(x)
-
+def updateCourses():
+    try:
+        for x in courses:
+            for y in selected:
+                if x['course'].lower() == y:
+                    selCourses.append(x)
+    except:
+        pass
+#updateCourses()
 
 def nextLecture():
     switch = False
     today = str(datetime.datetime.today()).replace(' ', '').replace(':', '').replace('.', '').split('-')
     today[2] = today[2][:6]
     for x in selCourses:
-        print x['dateVal']
-        print int(today[0] + today[1] + today[2])
+#        print x['dateVal']
+#        print int(today[0] + today[1] + today[2])
         if int(x['dateVal']) >= int(int(today[0] + today[1] + today[2])):
             switch = True
         if switch and (x['dateVal'] != None):
@@ -45,9 +50,23 @@ def timeplanDiv():
     return div
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('main.html', selCourses=selCourses, nextLecture = nextLecture, selected = selected)
+    if request.method == 'POST':
+        resp = make_response(render_template('main.html', selCourses=selCourses, nextLecture = nextLecture, selected = request.cookies.get('cookieCourse')))
+        global selected
+        selected = []
+        if len(request.form['activeCourses']) > 0:
+            resp.set_cookie('cookieCourse', request.form['activeCourses'])
+            selected.append(request.form['activeCourses'])
+        updateCourses()
+        return resp
+    if request.method == 'GET':
+        global selected
+        selected = []
+        selected.append(request.cookies.get('cookieCourse'))
+        updateCourses()
+    return render_template('main.html', selCourses=selCourses, nextLecture = nextLecture, selected = request.cookies.get('cookieCourse'))
 
 if __name__ == '__main__':
 #    app.run(debug=False, host="0.0.0.0")
